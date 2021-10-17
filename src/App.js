eff844d8a679c57119aec6860af7b5a949fa91de
Andrew Schaus <andrewschaus3@gmail.com>
2021-10-17 01:43:56 -0400

    Finished the lab

--- src/App.js
+++ src/App.js
@@ -1,27 +1,135 @@
 import React from 'react'; 
 import './App.css';
 
+let count = 0; //Needed this to be global
+
 export default class App extends React.Component
 {
   state = {
     rows: 6,
     columns: 7,
+    moves: [], //Starts the game with an empty board
+    playerTurn: 'red',
+  };
+
+  resetBoard = () => {
+    this.setState({ moves: [], winner: null, draw: false }) //Resets the board to empty and resets the winner
+    count = 0;
   }
 
+  getPeice = (x,y) => {
+    const list = this.state.moves.filter((item) => {
+      return (item.x === x && item.y === y);
+    });
 
-  
-  renderBoard() //Builds the board
+    return list[0];
+  }
+
+  checkForWin = (x,y, player) =>  //Functions checks for a winner by checking every time a move is made, it checks if the same colour is on either side or above or below
   {
-    const { rows, columns } = this.state;
+    let winnigMoves = [{ x, y }];
+    for(let column = x+1; column < x+4; column++)
+    {
+      const checkPeice = this.getPeice(column, y);
+      if(checkPeice && checkPeice.player === player){
+        winnigMoves.push({x: column, y: y});
+      }
+      else{
+        break;
+      } 
+    }
+
+    for(let column = x-1; column > x-4; column--)
+    {
+      const checkPeice = this.getPeice(column, y);
+      if(checkPeice && checkPeice.player === player){
+        winnigMoves.push({x: column, y: y});
+      }
+      else{
+        break;
+      } 
+    }
+
+    if(winnigMoves.length > 3)
+    {
+      this.setState({winner: player, winnigMoves});
+    }
+
+    winnigMoves = [{ x, y }];
+    for(let row = y+1; row < y+4; row++)
+    {
+      const checkPeice = this.getPeice(x, row);
+      if(checkPeice && checkPeice.player === player){
+        winnigMoves.push({x: x, y: row});
+      }
+      else{
+        break;
+      } 
+    }
+
+    for(let row = y-1; row > y-4; row--)
+    {
+      const checkPeice = this.getPeice(x, row);
+      if(checkPeice && checkPeice.player === player){
+        winnigMoves.push({x: x, y: row});
+      }
+      else{
+        break;
+      } 
+    }
+
+    if(winnigMoves.length > 3)
+    {
+      this.setState({winner: player, winnigMoves});
+    }
+ 
+  }
+
+  addMove = (x, y) => {
+
+    const { playerTurn } = this.state;
+    const nextPlayerTurn = playerTurn === 'red' ? 'yellow' : 'red'; //Switches player turn between red and yellow
+    let availableYPosition = null;
+    for(let position = this.state.rows-1; position >= 0; position--) //Finds the lowest avalible postion in the clicked on column
+    {
+      if(!this.getPeice(x, position))
+      {
+        availableYPosition = position;
+        break;
+      }
+    }
+
+    if(availableYPosition !== null)
+    {
+      this.setState({ moves: this.state.moves.concat({x, y: availableYPosition, player: playerTurn}), playerTurn: nextPlayerTurn}, () => this.checkForWin(x,availableYPosition,playerTurn)); //Adds a move to our state and changes player turn to the next player
+      count++;
+      console.log(count);
+    }
+
+    if(count === 42)
+    {
+      this.setState({draw: true});
+    }
+  }
+
+  renderBoard() //Builds the board 
+  {
+    const { rows, columns, winner, draw} = this.state;
     const rowsViews = [];
 
     for(let row = 0; row < this.state.rows; row += 1) //Loop that will go up to 6
     {
       const columnViews = [];
-      for(var column = 0; column < this.state.columns; column += 1) //Loop that will go up to 7
+      for(let column = 0; column < this.state.columns; column += 1) //Loop that will go up to 7
       {
+        const piece = this.getPeice(column, row);
         columnViews.push( //Setting the columns
-          <div style={{width: 100, height: 100, backgroundColor: 'black', border: '1px solid white'}}> </div>)
+          <div onClick={() => {this.addMove(column, row)}} style={{width: '8vw', height: '8vw', backgroundColor: 'black', display: 'flex', padding: 5, cursor: 'pointer'}}> 
+            <div style={{ borderRadius: '50%', display: 'flex', backgroundColor: 'white', flex: 1}}>
+              {piece ? <div style={{ backgroundColor: piece.player, flex: 1, borderRadius: '50%', border: '1px solid #333'}}/> : undefined}
+            </div>
+          </div>
+        );
       };
       rowsViews.push( //Setting the rows
         <div style={{ display: 'flex', flexDirection: 'row'}}>{columnViews} </div>
@@ -29,6 +137,10 @@ export default class App extends React.Component
     }
     return(
       <div style={{ backgroundColor: 'red' , display: 'flex', flexDirection: 'column'}} > 
+          {winner && <div onClick={this.resetBoard} style={{position: 'absolute', zIndex: 3, left: 0, right: 0, bottom: 0, top: 0, backgroundColor: 'blue',
+           justifyContent: 'center',display: 'flex', alignItems: 'center'}}>WIN!! click to reset</div>}
+           {draw && <div onClick={this.resetBoard} style={{position: 'absolute', zIndex: 3, left: 0, right: 0, bottom: 0, top: 0, backgroundColor: 'blue',
+           justifyContent: 'center',display: 'flex', alignItems: 'center'}}>DRAW click to reset</div>}
           {rowsViews}
       </div>
   )
@@ -40,6 +152,7 @@ export default class App extends React.Component
     return(
       <div style = {style ? Object.assign({}, styles.container, style) : styles.container}> 
         {this.renderBoard()}
+        <button onClick={this.resetBoard} style={{padding: 5}}>Clear Board</button> 
       </div>
     );
   }
